4857f113d48c9e47fed07f1eb44b2a3597e9a0af
Graham Gilchrist <git@grahamgilchrist.com>
2018-05-18 14:59:45 +0100

    Add utils and ship build model from hotac-ship-builder

---
+++ modules/models/shipBuild/itemTypes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+    SHIP_TYPE: 'ship',
+    PILOT_SKILL: 'pilotSkill',
+    BUY_UPGRADE: 'buyUpgrade',
+    LOSE_UPGRADE: 'loseUpgrade',
+    XP: 'xp',
+    MISSION: 'mission',
+    STARTING_SHIP_TYPE: 'startingShip',
+    BUY_PILOT_ABILITY: 'pilotAbility',
+    LOSE_PILOT_ABILITY: 'losePilotAbility'
+};
